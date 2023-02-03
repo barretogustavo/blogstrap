b34a45d7d90865562c4d720a26b9427ea4c0dc8a
Gustavo Barreto <gbarreto.alves2@gmail.com>
2023-02-02 22:58:54 -0300

    chore: change url in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Email
-  config.action_mailer.default_url_options = { host: 'blogstrap.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'gustavobarreto.fly.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
