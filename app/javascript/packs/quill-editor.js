8ce119f040e9b8ab3ee743b2b9cf45db0f497356
Gustavo Barreto <gbarreto.alves2@gmail.com>
2023-02-07 13:46:57 -0300

    feat: implementing text editor in new/edit article

---
+++ app/javascript/packs/quill-editor.js
@@ -0,0 +1,4 @@
+import { DirectUpload } from "@rails/activestorage"
+import ImageResize from "@taoqf/quill-image-resize-module/image-resize.min";
+import Quill from 'quill/quill';
+export default Quill;
