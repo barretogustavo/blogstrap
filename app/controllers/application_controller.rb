4b416f06ed41da4123206b31bec592c7e03e63fa
Gustavo Barreto <gbarreto.alves2@gmail.com>
2023-02-07 13:49:57 -0300

    fix: fix edit user bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   around_action :switch_locale
 
   private
 
-  # app/controllers/application_controller.rb
   def default_url_options
     { locale: I18n.locale }
   end
@@ -19,26 +19,7 @@ class ApplicationController < ActionController::Base
   def user_not_authorized(exception)
     policy_name = exception.policy.class.to_s.underscore
 
-    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
-    redirect_back(fallback_url: root_path)
-  end 
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to user_session_path, :alert => 'You need to sign in or sign up before continuing.'
-    end
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: 'pundit', default: :default
+    redirect_to(request.referrer || root_path)
   end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
-  end
-
 end
